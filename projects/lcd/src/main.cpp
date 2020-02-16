4b2353ea3fbc880c350144270cc055399590ef28
Muhammad Hamza Hanif <hamzaarain1999@gmail.com>
2020-02-17 01:15:48 +0500

    added *files

---
+++ projects/lcd/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  // initialize the LCD
+  lcd.begin();
+
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+  }
+
+void loop()
+{
+  // Do nothing here...
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Pressure Level: ");
+  
+  for(int i=0; i<= 10; i++){
+    lcd.setCursor(0,1);
+    lcd.print(476+i);
+    lcd.print(" MBars");
+    delay(500);    
+  }
+  
+}
